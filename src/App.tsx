0c5f1644b23375cf6b29c36217d5befd444849d4
Achim Lenz <hello.achim@outlook.com>
2023-12-08 10:51:01 +0100

    updated version

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,10 @@ import CharacterGallery from "./CharacterGallery.tsx";
 import {Route, Routes} from "react-router-dom";
 import Welcome from "./Welcome.tsx";
 import Header from "./Header.tsx";
-import CharacterDetails from "./CharacterDetails.tsx";
 
 function App() {
 
     const [characters, setCharacters] = useState<Character[]>(charactersResponse.results)
-
-    // <Route path={"/characters/:id"} element={<CharacterDetails character={characters.find((char) => char.id)}/>}/>
 
     return (
         <>
